7fc80ee88bc9fd3625a919e2bdb26aaa325b3b43
Rion <rionfx.42@gmail.com>
2021-08-16 18:06:44 +0900

    Created Project that Building Lists and Navigation

---
+++ LandmarksUI/LandmarksUI/Model/Landmark.swift
@@ -0,0 +1,35 @@
+//
+//  Landmark.swift
+//  LandmarksUI
+//
+//  Created by FX on 2021/08/16.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable, Identifiable{
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+        
+    private var coordinates: Coodinates
+    var locationCoodenate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+        
+    struct Coodinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
---
+++ LandmarksUI/LandmarksUI/Model/ModelData.swift
@@ -0,0 +1,33 @@
+//
+//  ModelData.swift
+//  LandmarksUI
+//
+//  Created by FX on 2021/08/16.
+//
+
+import Foundation
+
+var landmarks: [Landmark] = load("landmarkData.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't finc \(filename) in main bundle.")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
+ 
