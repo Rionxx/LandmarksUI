86d7b4b274937544099b3cc7215941c782c0a098
Rion <rionfx.42@gmail.com>
2021-08-16 16:06:59 +0900

    Created Project by Xcode(two)

---
+++ LandmarksUI/LandmarksUI/LandmarksUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksUIApp.swift
+//  LandmarksUI
+//
+//  Created by FX on 2021/08/16.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
